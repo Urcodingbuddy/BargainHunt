f2b460a36daa48976c32b068954cb7ebe9ea460d
Urcodingbuddy <anshpethe110@gmail.com>
2025-04-03 21:20:12 +0530

    refactor scraper to use puppeteer-core and remove unnecessary plugins

--- apps/web/lib/scraper.ts
+++ apps/web/lib/scraper.ts
@@ -1,7 +1,6 @@
-import puppeteer from "puppeteer-extra";
+import puppeteer from "puppeteer-core";
 import chromium from "@sparticuz/chromium";
-import StealthPlugin from "puppeteer-extra-plugin-stealth";
-puppeteer.use(StealthPlugin());
+
 
 chromium.setGraphicsMode = false;
 
