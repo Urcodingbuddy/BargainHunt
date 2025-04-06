b6e7e3cf989709eae96edb9b4455c221e19e8427
Urcodingbuddy <anshpethe110@gmail.com>
2025-04-06 10:52:34 +0530

    fix: add z-index to Chatbot component for proper stacking context

--- apps/web/components/Chatbot.tsx
+++ apps/web/components/Chatbot.tsx
@@ -7,7 +7,7 @@ import { BorderBeam } from "./ui/border-beam";
 export default function Chatbot() {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="relative overflow-hidden rounded-lg">
+    <div className="relative overflow-hidden rounded-lg z-20">
       <button
         onClick={() => setIsOpen(!isOpen)}
         className="h-12 md:h-14 w-12 md:w-14 rounded-full md:bg-purple-800 bg-purple-600 text-white fixed bottom-6 md:bottom-13 right-6 md:right-13 flex items-center justify-center shadow-lg hover:bg-purple-600 transition duration-300 cursor-pointer group ease-in-out"
@@ -19,7 +19,7 @@ export default function Chatbot() {
         initial={{ opacity: 0, scale: 0, transformOrigin: "bottom right" }}
         animate={isOpen ? { opacity: 1, scale: 1 } : { opacity: 0, scale: 0 }}
         transition={{ duration: 0.5, ease: "easeInOut" }}
-        className="fixed bottom-18 md:bottom-24 md:right-24 right-12 w-72 md:w-96  h-[60vh] md:h-[75vh] shadow-lg rounded-lg overflow-hidden origin-bottom-right
+        className="fixed bottom-18 md:bottom-24 md:right-24 right-9 w-72 md:w-96  h-[60vh] md:h-[75vh] shadow-lg rounded-lg overflow-hidden origin-bottom-right
         border-2"
       >
           <BorderBeam />
