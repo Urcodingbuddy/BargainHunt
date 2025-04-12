b56ee9a86da3d51ddd51eac9660e5f6907ba4741
Urcodingbuddy <anshpethe110@gmail.com>
2025-04-12 11:19:54 +0530

    feat(auth): enhance ModalWrapper to display user image and adjust button size based on session state

--- apps/web/components/Dynamic-popup.tsx
+++ apps/web/components/Dynamic-popup.tsx
@@ -1,13 +1,15 @@
+"use client"
 import { useState } from "react";
 import { Button } from "./ui/button";
 import dynamic from "next/dynamic";
 import { HiMiniUser } from "react-icons/hi2";
+import { useSession } from "next-auth/react";
 const Modal = dynamic(() => import("@/components/Pop-up"), { ssr: false });
 const SignupForm = dynamic(() => import("@/components/Auth"), { ssr: false });
 
 export default function ModalWrapper() {
     const [isSignupOpen, setIsSignupOpen] = useState(false);
-  
+    const { data: session } = useSession();
     const openSignup = () => setIsSignupOpen(true);
     const closeSignup = () => setIsSignupOpen(false);
   
@@ -17,9 +19,17 @@ export default function ModalWrapper() {
               variant="ghost"
               size="icon"
               onClick={openSignup}
-              className="rounded-full border-gray-700 border-2 flex items-end h-8 w-8 cursor-pointer text-white hover:bg-gray-900"
+              className={`rounded-full border-gray-700 hover:border-purple-600 border-2 flex justify-center items-center ${session ? "h-10 w-10" : "h-8 w-8"} cursor-pointer text-white hover:bg-gray-900`}
             >
-              <HiMiniUser className="w-6 h-6" />
+                {session?.user.image ? (
+                <img
+                  src={session.user.image}
+                  alt="User Image"
+                  className="rounded-full w-8 h-8"
+                />
+                ) : (
+                <HiMiniUser className="w-6 h-6" />
+                )}
             </Button>
         <Modal isOpen={isSignupOpen} onClose={closeSignup}>
           <SignupForm />
@@ -27,5 +37,3 @@ export default function ModalWrapper() {
       </>
     );
   }
-
-//   
