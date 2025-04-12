0450d33d9861bf5a491d303ed5c3dece0b0e6dc2
Urcodingbuddy <anshpethe110@gmail.com>
2025-04-12 11:19:19 +0530

    feat(auth): add TypeScript definitions for NextAuth user and session interfaces

---
+++ apps/web/app/next-auth.d.ts
@@ -0,0 +1,30 @@
+// types/next-auth.d.ts
+import NextAuth, { DefaultSession } from "next-auth";
+import { JWT } from "next-auth/jwt";
+
+declare module "next-auth" {
+  interface User {
+    id: string;
+    name: string;
+    phone?: string | null;
+    email: string;
+    auth_type?: "Google" | "Github";
+    imageUrl?: string | null;
+    createdAt?: Date | null;
+  }
+
+  interface Session extends DefaultSession {
+    user: User & { id: string };
+  }
+  
+interface Profile {
+  avatar_url?: string; // ✅ GitHub avatar URL
+  picture?: string; // ✅ Google profile picture
+}
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+  }
+}
