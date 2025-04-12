6f20a5225327a1686a96b399c4e5d93689b1e8b3
Urcodingbuddy <anshpethe110@gmail.com>
2025-04-12 13:47:10 +0530

    fix(auth): refactor signIn callback for improved user handling and error checking

--- apps/web/app/api/auth/authOptions.ts
+++ apps/web/app/api/auth/authOptions.ts
@@ -7,56 +7,55 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export const authOptions:AuthOptions = {
-    providers: [
-        GoogleProvider({
-          clientId: process.env.GOOGLE_CLIENT_ID!,
-          clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-        }),
-        GitHubProvider({
-          clientId: process.env.GITHUB_CLIENT_ID!,
-          clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-        }),
-      ],
-      
-      callbacks:{
-        async signIn({user, account, profile}){
-            const existingUser = await prisma.user.findUnique({
-                where: { email: user.email! },
-          });
+export const authOptions: AuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+  ],
 
-          if(!existingUser) {
-            await prisma.user.create({
-                data: {
-                    email: user.email!,
-                    name: user.name!,
-                    phone: user.phone!,
-                    imageUrl: profile?.avatar_url ?? profile?.picture ?? user.imageUrl ?? "",
-                    auth_type: account?.provider === "google" ? "GOOGLE" : "GITHUB",
-                    
-                },
-                
-            });
-          }
-          if (!existingUser?.id) {
-            throw new Error("User ID is missing");
-          }
-          user.id = existingUser.id;
-          return true
-      },
-      async jwt({ token, user }) {
-        if (user) {
-          token.id = user.id;
-        }
-        return token;
-      },
-      async session({ session, token }: { session: Session; token: JWT }) {
-        if (session.user) {
-          session.user.id = token.id;
-        }
-        return session;
-      },
-      async redirect({  baseUrl }: {  baseUrl: string }) {
-        return baseUrl + '/';
-    }    
-}}
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const existingUser = await prisma.user.findUnique({
+        where: { email: user.email! },
+      });
+      if (existingUser) {
+        user.id = existingUser.id;
+        return true;
+      }
+      if (!existingUser) {
+        await prisma.user.create({
+          data: {
+            email: user.email!,
+            name: user.name!,
+            phone: user.phone!,
+            imageUrl:
+              profile?.avatar_url ?? profile?.picture ?? user.imageUrl ?? "",
+            auth_type: account?.provider === "google" ? "GOOGLE" : "GITHUB",
+          },
+        });
+      }
+      return true;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }: { session: Session; token: JWT }) {
+      if (session.user) {
+        session.user.id = token.id;
+      }
+      return session;
+    },
+    async redirect({ baseUrl }: { baseUrl: string }) {
+      return baseUrl + "/";
+    },
+  },
+};
